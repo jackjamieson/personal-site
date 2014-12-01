d4adbb727b2e217bdb73563923f6116291aeb46b
jackjamieson <jamieson.jack@gmail.com>
2014-11-30 21:09:15 -0500

    finished new website for new domain

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,3 @@
-
 var divSide = document.getElementById('sidebar');
 divSide.innerHTML = 	'' +
 	'<div class="header">' +
@@ -17,5 +16,5 @@ divNav.innerHTML = 	'' +
 	'&#8594; <a href="index.html">About</a><br>' +
 	'&#8594; <a href="resume.pdf">Resume</a><br>' +
 	'&#8594; <a href="projects.html">Projects</a><br>' +
-	'';
+    '<div id="logos"><a href="https://github.com/jackjamieson" target="_blank" class="imga"><img src="img/git.png"/></a> ' + ' <a href="https://www.linkedin.com/pub/jack-jamieson/3a/256/367" target="_blank" class="imga"><img src="img/in.png"/></a> '+ ' <a href="https://twitter.com/jamieson_jack" target="_blank" class="imga"><img src="img/tw.png"/></a></div>' ;
 
