5ccd15c7d4b675da3def4d46bdd1594a15d1be66
Jack Jamieson <jamieson.jack@gmail.com>
2014-11-30 19:21:53 -0500

    new site for new domain

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+
+var divSide = document.getElementById('sidebar');
+divSide.innerHTML = 	'' +
+	'<div class="header">' +
+				'<center><img src="img/photo.jpg" class="photo"/></center>' +
+				'Jack Jamieson' +
+				'<div id="nav">' +
+
+				'</div>' +
+
+			'</div>' +
+	'';
+
+
+var divNav = document.getElementById('nav');
+divNav.innerHTML = 	'' +
+	'&#8594; <a href="index.html">About</a><br>' +
+	'&#8594; <a href="resume.pdf">Resume</a><br>' +
+	'&#8594; <a href="projects.html">Projects</a><br>' +
+	'';
+
