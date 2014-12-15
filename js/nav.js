27850493a4f351215ecea10f2227c89c9d639de6
jackjamieson <jamieson.jack@gmail.com>
2014-12-15 14:01:26 -0500

    added google analytics to main site

--- js/nav.js
+++ js/nav.js
@@ -18,3 +18,14 @@ divNav.innerHTML = 	'' +
 	'&#8594; <a href="projects.html">Projects</a><br>' +
     '<div id="logos"><a href="https://github.com/jackjamieson" target="_blank" class="imga"><img src="img/git.png"/></a> ' + ' <a href="https://www.linkedin.com/pub/jack-jamieson/3a/256/367" target="_blank" class="imga"><img src="img/in.png"/></a> '+ ' <a href="https://twitter.com/jamieson_jack" target="_blank" class="imga"><img src="img/tw.png"/></a></div>' ;
 
+
+//Google Analytics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40920141-1', 'auto');
+  ga('send', 'pageview');
+
+
