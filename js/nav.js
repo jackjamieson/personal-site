22867604db395fe335a2a4521e063789afa14f5f
jackjamieson <jamieson.jack@gmail.com>
2015-08-18 15:11:48 -0400

    removed auto moving links

--- js/nav.js
+++ js/nav.js
@@ -13,17 +13,7 @@ divSide.innerHTML = 	'' +
 
 var divNav = document.getElementById('nav');
 divNav.innerHTML = 	'' +
-	'&#8594; <a href="index.html#abcontent">About</a><br>' +
+	'&#8594; <a href="index.html">About</a><br>' +
 	'&#8594; <a href="JackJamiesonResume.pdf">Resume</a><br>' +
-	'&#8594; <a href="projects.html#projcontent">Projects</a><br>' +
+	'&#8594; <a href="projects.html">Projects</a><br>' +
     '<div id="logos"><a href="https://github.com/jackjamieson" class="imga"><img src="img/git.png"/></a> ' + ' <a href="https://www.linkedin.com/pub/jack-jamieson/3a/256/367"  class="imga"><img src="img/in.png"/></a> '+ ' <a href="https://twitter.com/jamieson_jack"  class="imga"><img src="img/tw.png"/></a></div>' ;
-
-
-//Google Analytics
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-40920141-1', 'auto');
-  ga('send', 'pageview');
